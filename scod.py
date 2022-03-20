b9c2a3e3087eaf534e951059fda4f95d63c2d2c6
Alvin Sun <alvinsunyixiao@gmail.com>
2022-03-19 23:07:49 -0700

    add diagonal approximation

--- scod.py
+++ scod.py
@@ -1,3 +1,4 @@
+import numpy as np
 import typing as T
 import tensorflow as tf
 import tensorflow.keras as tfk
@@ -7,54 +8,65 @@ from tqdm import tqdm, trange
 
 from distribution import OutputDist
 from sketching import Sketch, SketchOp, SRFTSketchOp
-from priors import WeightPrior, PerVarPrior
 
 class SCOD(tfk.Model):
     def __init__(self,
                  model: tfk.Model,
                  output_dist: OutputDist,
-                 num_samples: int,
                  model_vars: T.Optional[T.List[tf.Variable]] = None,
                  num_eigs: int = 50,
                  sketch_op_class: T.Type[SketchOp] = SRFTSketchOp,
-                 prior_class: T.Type[WeightPrior] = PerVarPrior,
+                 output_func: T.Optional[T.Callable[..., tf.Tensor]] = None,
+                 diag: bool = True,
+                 log_prior_init: np.ndarray = np.ones(1),
                  *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.base_model = model
         self.output_dist = output_dist
+
+        self.output_func = output_func
+        if output_func is None:
+            self.output_func = lambda x: x
 
         self.model_vars = model_vars
         if model_vars is None:
             self.model_vars = model.trainable_variables
         model_vars_shps = tf.nest.map_structure(tf.size, self.model_vars)
         self.num_vars = tf.reduce_sum(model_vars_shps).numpy()
-
-        self.num_samples = num_samples
+        print(f"Calibrating for {self.num_vars} variables:")
+        print("\n".join([var.name for var in self.model_vars]))
 
         self.sketch = Sketch(
             N=self.num_vars,
-            M=self.num_samples,
             k=num_eigs,
             sketch_op_class=sketch_op_class,
         )
-        self.prior = prior_class(self.model_vars)
+        self.diag = diag
+        self.log_prior = self.add_weight(name="log_prior", shape=log_prior_init.shape,
+                                         initializer=tfk.initializers.constant(log_prior_init))
 
-    def process_dataset(self, dataset: tf.data.Dataset):
+    def process_dataset(self, dataset: tf.data.Dataset, sketch_dir: str = "/tmp/sketch"):
         for x in tqdm(dataset):
-            batch_L_w = self._compute_sqrt_fisher_w(x)
-            self.sketch.batch_update(batch_L_w)
+            self._update_sqrt_fisher_w(x)
+
         self.sketch.fixed_rank_eig_approx()
 
     @tf.function
-    def _compute_sqrt_fisher_w(self, x):
+    def _update_sqrt_fisher_w(self, x):
         with tf.GradientTape() as tape:
             f = self.base_model(x)
             g = self.output_dist.apply_sqrt_fisher(f)
-        batch_jacobian_list = tape.jacobian(g, self.model_vars)
-        batch_jacobian = self._batch_jacobian_concat(batch_jacobian_list)
-        batch_L_w = tf.transpose(batch_jacobian, (0, 2, 1))
 
-        return batch_L_w
+        batch_jacobian = tape.jacobian(g, self.model_vars)
+
+        def partial_flatten(input):
+            shp = tf.shape(input)
+            return tf.reshape(input, (shp[0], -1))
+
+        for i in tf.range(tf.shape(g)[0]):
+            jacobians = tf.nest.map_structure(lambda x: partial_flatten(x[i]), batch_jacobian)
+            LT = tf.concat(jacobians, axis=-1)
+            self.sketch.update(LT)
 
     def _batch_jacobian_concat(self, batch_jac_list: T.List[tf.Variable]):
         def partial_flatten(input):
@@ -68,16 +80,20 @@ class SCOD(tfk.Model):
     def _optimize_prior_once(self, data, sigma_scale):
         inputs, labels = data
         with tf.GradientTape() as tape:
-            y, S = self(inputs)
+            output, S = self(inputs)
+            y = self.output_func(output)
             distribution = tfd.MultivariateNormalTriL(y, sigma_scale * tf.linalg.cholesky(S))
             if tf.rank(labels) == 1:
                 labels = labels[:, tf.newaxis]
             log_ll = distribution.log_prob(labels)
             loss = tf.reduce_mean(-log_ll)
 
-        grads = tape.gradient(loss, self.prior.log_priors)
+        grads = tape.gradient(loss, self.log_prior)
 
         return loss, grads
+
+    def load_from_saved_model(self, path):
+        saved_model = tfk.models.load_model(path)
 
     def calibrate_prior(self,
         dataset: tf.data.Dataset,
@@ -94,7 +110,7 @@ class SCOD(tfk.Model):
             cnt = 0
             for data in dataset:
                 loss, grads = self._optimize_prior_once(data, sigma_scale)
-                optimizer.apply_gradients([(grads, self.prior.log_priors)])
+                optimizer.apply_gradients([(grads, self.log_prior)])
                 sum_loss += loss
                 cnt += 1
             losses.append((sum_loss / cnt).numpy())
@@ -104,10 +120,15 @@ class SCOD(tfk.Model):
 
     def call(self, x):
         with tf.GradientTape() as tape:
-            y = self.base_model(x)
-        batch_jacobian_list = tape.jacobian(y, self.model_vars)
+            output = self.base_model(x)
+            g = self.output_dist.apply_sqrt_fisher(output)
+        batch_jacobian_list = tape.jacobian(g, self.model_vars)
         batch_jacobian = self._batch_jacobian_concat(batch_jacobian_list)
-        sqrt_prior = self.prior.broadcast()
 
-        return y, self.sketch.cov_posterior(batch_jacobian, sqrt_prior)
+        if self.diag:
+            S = self.sketch.diag_var_posterior(batch_jacobian, self.log_prior)
+        else:
+            S = self.sketch.cov_posterior(batch_jacobian, self.log_prior)
+
+        return output, S
 
--- sketching.py
+++ sketching.py
@@ -16,10 +16,10 @@ class SketchOp(tf.Module):
 class GaussianSketchOp(SketchOp):
     def __init__(self, d: int, N: int):
         super().__init__(d, N)
-        self.op = tf.Variable(tf.random.normal((d, N)), trainable=False)
+        self.op = tf.Variable(tf.random.normal((N, d)), trainable=False)
 
-    def __call__(self, M: tf.Tensor, batched: bool = False):
-        return self.op @ M
+    def __call__(self, M: tf.Tensor) -> tf.Tensor:
+        return M @ self.op
 
 class SRFTSketchOp(SketchOp):
     def __init__(self, d: int, N: int):
@@ -27,22 +27,21 @@ class SRFTSketchOp(SketchOp):
         self.D = tf.Variable(tfp.random.rademacher((N,)), trainable=False)
         self.P = tf.Variable(np.random.choice(N, d, replace=False), trainable=False)
 
-    def __call__(self, M: tf.Tensor):
+    def __call__(self, M: tf.Tensor) -> tf.Tensor:
         # apply diag(D)
-        D_M = self.D[:, tf.newaxis] * M
+        D_M = self.D * M
         # perform inverse discrete consine transform to the first axis
-        F_D_M = tf.transpose(tf.signal.idct(tf.transpose(D_M)))
+        F_D_M = tf.signal.idct(D_M)
         # randomly chooses n rows
-        return tf.gather(F_D_M, self.P)
+        return tf.gather(F_D_M, self.P, axis=1)
 
 
 class Sketch(tf.Module):
-    def __init__(self, N: int, M: int, k: int,
+    def __init__(self, N: int, k: int,
                  T: typing.Optional[int] = None,
                  sketch_op_class: typing.Type[SketchOp] = SRFTSketchOp):
         super().__init__(name=self.__class__.__name__)
         self.N = N
-        self.M = M
         self.k = k
         self.T = T
         if T is None:
@@ -61,28 +60,27 @@ class Sketch(tf.Module):
         self.eigs = tf.Variable(tf.zeros(self.k), trainable=False)
         self.basis = tf.Variable(tf.zeros((self.N, self.k)), trainable=False)
 
-    @tf.function
-    def update(self, L: tf.Tensor):
-        self.Y.assign_add(tf.matmul(L, self.Omega(L), transpose_b=True))
-        self.W.assign_add(tf.matmul(self.Psi(L), L, transpose_b=True))
+    def update(self, LT: tf.Tensor):
+        self.Y.assign_add(tf.matmul(LT, self.Omega(LT), transpose_a=True))
+        self.W.assign_add(tf.matmul(self.Psi(LT), LT, transpose_a=True))
 
-    # TODO(alvin): vectorize this maybe?
-    @tf.function
-    def batch_update(self, batch_L: tf.Tensor):
-        for L in batch_L:
-            self.update(L)
+    def batch_update(self, batch_LT: tf.Tensor):
+        for LT in batch_LT:
+            self.update(LT)
 
     def fixed_rank_eig_approx(self):
         # A ~= QX
         Q, _ = tf.linalg.qr(self.Y)
-        U, T = tf.linalg.qr(self.Psi(Q))
+        U, T = tf.linalg.qr(tf.linalg.adjoint(self.Psi(tf.linalg.adjoint(Q))))
         X = tf.linalg.triangular_solve(T, tf.transpose(U) @ self.W)
+        tf.print("A ~= QX Done")
 
         # A ~= U S U_T (where S is symmetric)
         U, T = tf.linalg.qr(tf.concat([Q, tf.transpose(X)], axis=1))
         T1 = T[:, :self.r]
         T2 = T[:, self.r:]
         S = (T1 @ tf.transpose(T2) + T2 @ tf.transpose(T1)) / 2.
+        tf.print("A ~= U S U_T Done")
 
         # A ~= U D U_T (where D is diagonal)
         D, V = tf.linalg.eigh(S)
@@ -91,21 +89,25 @@ class Sketch(tf.Module):
         V = V[:, -self.k:]
         D = tf.maximum(D, 0.0) # ensures PSD
         U = U @ V
+        tf.print("A ~= U D U_T Done")
 
         self.eigs.assign(D)
         self.basis.assign(U)
 
-    def cov_posterior(self, batch_jac: tf.Tensor, sqrt_prior: tf.Tensor):
-        scaled_batch_jac = batch_jac * sqrt_prior[tf.newaxis, tf.newaxis]
-        JT_S_J = tf.matmul(scaled_batch_jac, scaled_batch_jac, transpose_b=True)
+    def cov_posterior(self, batch_jac: tf.Tensor, log_prior: tf.Tensor):
+        J_JT = tf.matmul(batch_jac, batch_jac, transpose_b=True)
 
-        UT_S = tf.transpose(self.basis) * sqrt_prior[tf.newaxis]**2
-        UT_S_U = UT_S @ self.basis
-        A = tf.linalg.diag(1.0 / self.eigs) + UT_S_U
-        L = tf.linalg.cholesky(A)
-        Linv_UT_S = tf.linalg.triangular_solve(L, UT_S)
-        scaled_batch_jac_2 = tf.matmul(batch_jac, Linv_UT_S[tf.newaxis], transpose_b=True)
-        JT_S2_J = tf.matmul(scaled_batch_jac_2, scaled_batch_jac_2, transpose_b=True)
+        J_U = batch_jac @ self.basis
+        J2 = J_U * tf.sqrt(self.eigs / (self.eigs + tf.exp(-2 * log_prior)))
+        J2_J2T = tf.matmul(J2, J2, transpose_b=True)
 
-        return JT_S_J - JT_S2_J
+        return tf.exp(2 * log_prior) * (J_JT - J2_J2T)
 
+    def diag_var_posterior(self, batch_jac: tf.Tensor, log_prior: tf.Tensor):
+        J_JT = tf.reduce_sum(tf.square(batch_jac), axis=-1)
+
+        J_U = batch_jac @ self.basis
+        J_U_S = J_U * (self.eigs / (self.eigs + tf.exp(-2 * log_prior[..., tf.newaxis])))
+        J2_J2T = tf.reduce_sum(J_U_S * J_U, axis=-1)
+
+        return tf.exp(2 * log_prior) * (J_JT - J2_J2T)
